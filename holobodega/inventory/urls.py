078c4993b5c47a4b39bd22f46389dc32477515f9
unknown <erickp1502@gmail.com>
2022-04-24 19:09:00 -0500

    feat: search products and categories

--- holobodega/inventory/urls.py
+++ holobodega/inventory/urls.py
@@ -3,7 +3,15 @@ from inventory.views import *
 
 urlpatterns = [
     # PRODUCTOS
+    path('product/', products),
     path('new_product/', new_product),
+    path('product/delete/<id>/',delete_product,name="delete_product"),
+    path('product/search/',search_product,name="search_product"),
     #CATEGORÍAS
     # path('new_category', new_category),
+    path('category/', categories),
+    path('category/delete/<id>/',delete_category,name="delete_category"),
+    path('category/search/',search_category,name="search_category")
+    
+    
 ]
--- holobodega/inventory/views.py
+++ holobodega/inventory/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import redirect, render
-
+from multiprocessing import context
+from django.shortcuts import redirect, render,get_object_or_404
+from django.db.models import *
 from inventory.forms import NewProductForm
-from inventory.models import Category, Producer, Product
-
+from inventory.models import *
 # Create your views here.
 
 def new_product(request):
@@ -30,3 +30,45 @@ def new_product(request):
     }
 
     return render(request, 'inventory/product_form.html', context)
+    
+def categories (req):
+  categories=Category.objects.all()
+  context ={
+    'categories':categories
+  }
+  return render(req, 'inventory/categories.html',context)
+
+def search_category(req):
+  if req.method=='POST':
+    name=req.POST.get("search_input")
+  categories=Category.objects.filter(name__contains=name)
+  context={
+    'categories':categories
+  }
+  return render(req,'inventory/categories.html',context)
+
+def delete_category (req, id):
+  category=get_object_or_404(Category,id=id)
+  category.delete()
+  return redirect(categories)
+
+def delete_product (req, id):
+  product=get_object_or_404(Product,id=id)
+  product.delete()
+  return redirect(products)
+
+def products (req):
+  products=Product.objects.all()
+  context ={
+    'products':products
+  }
+  return render(req, 'inventory/products.html',context)
+
+def search_product(req):
+  if req.method=='POST':
+    name=req.POST.get("search_input")
+  products=Product.objects.filter(name__contains=name)
+  context={
+    'products':products
+  }
+  return render(req,'inventory/products.html',context)
