fba2d9f319b7260965f504265c75550231df36b8
unknown <epalomino@sanms.com.pe>
2022-04-25 15:52:18 -0500

    fix: sale-detail styles and more models to admin

--- holobodega/sale/admin.py
+++ holobodega/sale/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from sale.models import Debtor
+from sale.models import *
 
 # Register your models here.
 admin.site.register(Debtor)
+admin.site.register(DebtSale)
+admin.site.register(Sale)
+admin.site.register(SaleLine)
